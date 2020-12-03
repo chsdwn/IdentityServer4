f9c9c90552776f108b43eabc7c0d2f7d14362041
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-12-03 16:28:57 +0300

    12 - Client tanımlamaları

--- AuthServer/Config.cs
+++ AuthServer/Config.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using IdentityServer4.Models;
 
 namespace AuthServer
 {
     public static class Config
     {
+        // API'lar
         public static IEnumerable<ApiResource> GetApiResources()
         {
             return new List<ApiResource>
@@ -20,6 +22,7 @@ namespace AuthServer
             };
         }
 
+        // API izinleri
         public static IEnumerable<ApiScope> GetApiScopes()
         {
             return new List<ApiScope>
@@ -30,6 +33,32 @@ namespace AuthServer
                 new ApiScope("api2.read", "API 2 için okuma izni"),
                 new ApiScope("api2.write", "API 2 için yazma izni"),
                 new ApiScope("api2.update", "API 2 için güncelleme izni")
+            };
+        }
+
+        // API'ları kullanacak Client'lar
+        public static IEnumerable<Client> GetClients()
+        {
+            var a = new List<string> { "", "" };
+
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "Client1",
+                    ClientName = "Client 1 WebApp",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1.read", "api2.write", "api2.update" }
+                },
+                new Client
+                {
+                    ClientId = "Client2",
+                    ClientName = "Client 2 WebApp",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1.read", "api2.write", "api2.update" }
+                }
             };
         }
     }
