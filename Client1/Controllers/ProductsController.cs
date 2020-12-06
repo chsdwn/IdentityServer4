02050bd79166b74e3e568beaa76f88a1416c101c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-12-06 13:25:43 +0300

    20 - Client1'de client credentials token oluşturulup, API1'den product verileri alındı.

---
+++ Client1/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Client1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+
+            // Discovery endpoint: /.well-known/openid-configuration
+            var discovery = await httpClient.GetDiscoveryDocumentAsync("https://localhost:4001");
+            if (discovery.IsError)
+                Console.WriteLine(discovery.Error);
+
+            var tokenReq = new ClientCredentialsTokenRequest();
+            tokenReq.ClientId = _configuration["Client:ClientId"];
+            tokenReq.ClientSecret = _configuration["Client:ClientSecret"];
+            tokenReq.Address = discovery.TokenEndpoint; // /connect/token
+
+            var token = await httpClient.RequestClientCredentialsTokenAsync(tokenReq);
+            if (token.IsError)
+                Console.WriteLine(token.Error);
+
+            httpClient.SetBearerToken(token.AccessToken);
+
+            var response = await httpClient.GetAsync("https://localhost:5001/api/products/getproducts");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                Console.WriteLine(response.RequestMessage);
+            }
+
+            return View();
+        }
+    }
+}
