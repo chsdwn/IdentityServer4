973d0d39294943fc1baaa8bf77b1ba855391523a
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-12-07 15:38:35 +0300

    48 - Custom user repository method'ları oluşturuldu.

--- AuthServer/Services/CustomUserRepository.cs
+++ AuthServer/Services/CustomUserRepository.cs
@@ -1,7 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using AuthServer.Data;
+using AuthServer.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuthServer.Services
 {
-    public class CustomUserRepository
+    public class CustomUserRepository : ICustomUserRepository
     {
-        
+        private readonly AppDbContext _dbContext;
+
+        public CustomUserRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<CustomUser> FindByEmailAsync(string email)
+        {
+            return await _dbContext.CustomUsers.FirstOrDefaultAsync(c => c.Email.Equals(email));
+        }
+
+        public async Task<CustomUser> FindByIdAsync(int id)
+        {
+            return await _dbContext.CustomUsers.FindAsync(id);
+        }
+
+        public async Task<bool> ValidateAsync(string email, string password)
+        {
+            return await _dbContext.CustomUsers
+                .AnyAsync(c => c.Email.Equals(email) && c.Password.Equals(password));
+        }
     }
 }
